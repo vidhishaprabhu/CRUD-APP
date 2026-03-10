b1839744b4057c226b2d8749e59844bc31252b5f
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-10 13:16:02 +0530

    Created the student model

---
+++ models/Student.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose');
+const studentSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  age:{
+    type:Number,
+    required:true
+  },
+  email:{
+    type:Number,
+    required:true,
+    unique:true
+  },
+  studentClass:{
+    type:String,
+    required:true
+  },
+  address:{
+    type:String,
+    required:true
+  },
+  phone:{
+    type:String,
+    required:true
+  }
+},
+{
+  timestamps:true
+}
+)
